aeaab01a8ca7f5b3e9ba70a14729414854c09c77
Kubik2000 <jakub.drs@gmail.com>
2015-09-26 15:32:59 +0200

    Upgrade to VS2015 .net 4.6
    Recompiled in Microsoft Visual Studio 2015 Community  with .net
    framework 4.6

---
+++ PEProblem016/PEProblem016/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/*
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * 
+ * What is the sum of the digits of the number 2^1000?
+ */
+namespace PEProblem016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime StartTime = DateTime.Now;
+            
+            int[] array = new int[400];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+
+            array[0] = 2;
+            int tmp = 0;
+            int tmp2 = 0;
+
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    tmp = array[j] * 2 + tmp2;
+                    tmp2 = tmp / 10;
+                    array[j] = tmp - tmp2 * 10;
+                }
+            }
+
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+
+            TimeSpan elapsedTime = DateTime.Now - StartTime;
+
+            Console.WriteLine("result: " + result.ToString());
+            Console.WriteLine("elapsed time: " + elapsedTime.ToString("mm':'ss':'fff") + " mm:ss:fff");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# PEProblem016
+PEProblem016
+============
+This is a project Euler problem number 016
+It has been writen in 
+C#
+Visual Studio Community 2015
+.net framework 4.6
+
+I strongly suggest to use this only once you have  finished solving the problem.
